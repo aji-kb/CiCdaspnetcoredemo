730bdb09f10097ddaba0e86f00f78f0f9942acd0
ajitha-kb <hemadish@Adishs-MacBook-Pro.local>
2020-02-05 09:16:13 +0530

    compile error fix

--- cicdaspnetcoredemo.data/UserService.cs
+++ cicdaspnetcoredemo.data/UserService.cs
@@ -11,7 +11,7 @@ namespace cicdaspnetcoredemo.data
             this.users = new List<User> {
                 new User { Name="A", PhoneNumber="100"},
                 new User { Name="B", PhoneNumber="200"},
-                new User { Name="C", PhoneNumber="300"}
+                new User { Name="C", PhoneNumber="300"},
                 new User { Name="D", PhoneNumber="400"}
             };
         }
